b5e1b81914146302fab326c34d21080a6619c551
Luiz Avila <lapavila@gmail.com>
2012-07-04 10:04:05 -0300

    Work with signature webservice

--- bam/src/main/java/br/com/oncast/bam/service/xyzmo/XyzmoWebServiceNative.java
+++ bam/src/main/java/br/com/oncast/bam/service/xyzmo/XyzmoWebServiceNative.java
@@ -1,15 +1,39 @@
 package br.com.oncast.bam.service.xyzmo;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.StringWriter;
 import java.rmi.RemoteException;
 
-import org.apache.axis2.AxisFault;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
-import sign.webservicenativeprototype.inputinterface.server.xyzmo.com.SignServiceNativeStub;
 import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.DisposeDocument;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.DisposeDocumentResponse;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.DisposeDocumentResult;
 import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.GetActivities;
 import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.GetActivitiesResponse;
 import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.GetActivityConfigurationTemplate;
 import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.GetActivityConfigurationTemplateResponse;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.UploadDocument;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.UploadDocumentResponse;
+import management.webservicenative.inputinterface.server.xyzmo.com.ManagementServiceNativeStub.UploadDocumentResult;
+
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.databinding.types.UnsignedInt;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import sign.webservicenativeprototype.inputinterface.server.xyzmo.com.SignServiceNativeStub;
 
 public class XyzmoWebServiceNative {
 
@@ -37,6 +61,78 @@ public class XyzmoWebServiceNative {
 		template.setActivity(activityOfInterest);
 		GetActivityConfigurationTemplateResponse resGetConfig =	managementStub.getActivityConfigurationTemplate(template);
 		return resGetConfig.getGetActivityConfigurationTemplateResult();
+	}
+
+	public String modifyConfiguration(String activityConfiguration) throws Exception {
+		// modify the given XML configuration template by adding individual values
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder document = factory.newDocumentBuilder();
+		Document doc = document.parse(new ByteArrayInputStream(activityConfiguration.getBytes()));
+		
+		// edit the horizontal margin of the xyzmo seal
+		NodeList nodes = doc.getElementsByTagName("positionHorizontalMargin");
+		Node toChange = nodes.item(0);
+		Element toAdd = doc.createElement("positionHorizontalMargin");
+		toAdd.appendChild(doc.createTextNode("100"));
+		Node parent = toChange.getParentNode();
+		parent.replaceChild(toAdd, toChange);
+		
+		// edit the vertical margin of the xyzmo seal
+		nodes = doc.getElementsByTagName("positionVerticalMargin");
+		toChange = nodes.item(0);
+		toAdd = doc.createElement("positionVerticalMargin");
+		toAdd.appendChild(doc.createTextNode("100"));
+		parent = toChange.getParentNode();
+		parent.replaceChild(toAdd, toChange);
+		
+		// rewrite xml-documtent as string
+		Transformer transform = TransformerFactory.newInstance().newTransformer();
+		StreamResult result = new StreamResult(new StringWriter());
+		DOMSource source = new DOMSource(doc);
+		transform.transform(source, result);
+		return result.getWriter().toString();
+	}
+
+	public String uploadDocument(String documentoToUpload) throws RemoteException {
+		// read the document to sign from a location
+		File file = new File(documentoToUpload);
+		
+		// create a file container for uploading to the SIGNificant Server
+		ManagementServiceNativeStub.FileContainer container = new
+		ManagementServiceNativeStub.FileContainer();
+		FileDataSource filedata = new FileDataSource(file);
+		DataHandler handler = new DataHandler(filedata);
+		container.setSourceFileName(file.getName());
+		
+		container.setSourceFileContent(handler);
+		
+		// upload the document to sign, add a timespan how long the server should make available the document
+		UploadDocument uploadDoc = new UploadDocument();
+		uploadDoc.setDocument(container);
+		uploadDoc.setTimeSpan(new UnsignedInt(10));
+		UploadDocumentResponse resUploadDoc = managementStub.uploadDocument(uploadDoc);
+		UploadDocumentResult uploadDocResult = resUploadDoc.getUploadDocumentResult();
+		return uploadDocResult.getDocId();
+	}
+
+	public Boolean disposeDocument(String documentID) throws RemoteException {
+		// dispose the document from the server
+		DisposeDocument disposeDoc = new DisposeDocument();
+		disposeDoc.setId(documentID);
+		DisposeDocumentResponse resDisponse = managementStub.disposeDocument(disposeDoc);
+		DisposeDocumentResult disponseResult = resDisponse.getDisposeDocumentResult();
+		return disponseResult.getDisposed();
+	}
+
+	public String getServerInformation() throws RemoteException {
+		ManagementServiceNativeStub.GetServerInformation request = new ManagementServiceNativeStub.GetServerInformation();
+		ManagementServiceNativeStub.GetServerInformationResponse response = managementStub.getServerInformation(request);
+		return response.getGetServerInformationResult();
+	}
+
+	public Boolean Process(String documentID) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- bam/src/test/java/br/com/oncast/bam/service/xyzmo/XyzmoWeServiceNativeTest.java
+++ bam/src/test/java/br/com/oncast/bam/service/xyzmo/XyzmoWeServiceNativeTest.java
@@ -1,9 +1,9 @@
 package br.com.oncast.bam.service.xyzmo;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.rmi.RemoteException;
 
@@ -20,49 +20,60 @@ public class XyzmoWeServiceNativeTest {
 		webService = new XyzmoWebServiceNative();
 	}
 	
+	private String getFileContent(String fileName) throws FileNotFoundException, IOException {
+		FileInputStream inputStream = new FileInputStream(this.getClass().getResource("/" + fileName).getPath());
+		String expectedConfiguration;
+		byte[] readBytes = new byte[inputStream.available()];
+		inputStream.read(readBytes);
+		expectedConfiguration = new String(readBytes);
+		inputStream.close();
+		return expectedConfiguration;
+	}
+	
 	@Test
 	public void testGetAvailableActivities() throws RemoteException {
 		String[] expectedActivities = new String[] {"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfsecurityactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7activity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoaddattachmentactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoflattenpdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfexternalfinishactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfexternalprepareactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoanalyzeidentitycardactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoanalyzedocumentactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertexcel2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfabcactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfhdactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfwactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertimage2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2jpgactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2pngactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2tiffactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpowerpoint2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverttext2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertword2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertxml2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoword2pdfactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmogetsealpreviewactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmomail2htmlactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmomergepdfwithformdataactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmomimemergeractivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmomimeparseractivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2pdfaactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmovalidatepdf2pdfaactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7externalfinishactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7externalprepareactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealsimpleactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealexternalfinishactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealexternalprepareactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosendmailactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosigstringparsingactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosmimeactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmosmtpsendactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigexternalfinishactivity",
-				"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigexternalprepareactivity"};
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfsecurityactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7activity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoaddattachmentactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoflattenpdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfexternalfinishactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoadobepdfexternalprepareactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoanalyzeidentitycardactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoanalyzedocumentactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertexcel2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfabcactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfhdactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverthtml2pdfwactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertimage2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2jpgactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2pngactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2tiffactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpowerpoint2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconverttext2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertword2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertxml2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoword2pdfactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmogetsealpreviewactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmomail2htmlactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmomergepdfwithformdataactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmomimemergeractivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmomimeparseractivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoconvertpdf2pdfaactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmovalidatepdf2pdfaactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7externalfinishactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmopkcs7externalprepareactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealsimpleactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealexternalfinishactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosealexternalprepareactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosendmailactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosigstringparsingactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosmimeactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmosmtpsendactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigexternalfinishactivity",
+													"com.xyzmo.server.xyzmoactivitylibrary.xyzmoxmldsigexternalprepareactivity"};
+		
 		String[] availableActivities = webService.getAvailableActivities();
 		assertArrayEquals(expectedActivities, availableActivities);
 	}
@@ -71,12 +82,7 @@ public class XyzmoWeServiceNativeTest {
 	@Test
 	public void testGetActivityConfigurationTemplate() throws IOException {
 		
-		FileInputStream inputStream = new FileInputStream(this.getClass().getResource("/ActivityConfiguration.xml").getPath());
-		String expectedConfiguration;
-		byte[] readBytes = new byte[inputStream.available()];
-		inputStream.read(readBytes);
-		expectedConfiguration = new String(readBytes);
-		inputStream.close();
+		String expectedConfiguration = getFileContent("ActivityConfiguration.xml");
 		
 		String activityOfInterest = "com.xyzmo.server.xyzmoActivityLibrary.xyzmoSealSimpleActivity";
 		String activityConfiguration = webService.getActivityConfigurationTemplate(activityOfInterest);
@@ -84,7 +90,40 @@ public class XyzmoWeServiceNativeTest {
 	}
 	
 	@Test
-	public void testModifyConfiguration() {
+	public void testModifyConfiguration() throws Exception {
+		String expectedConfiguration = getFileContent("ModifiedActivityConfiguration.xml");
 		
+		String activeConfiguration = webService.getActivityConfigurationTemplate("com.xyzmo.server.xyzmoActivityLibrary.xyzmoSealSimpleActivity");
+		String modifiedConfiguration = webService.modifyConfiguration(activeConfiguration);
+		assertEquals(expectedConfiguration, modifiedConfiguration);
+	}
+	
+	@Test
+	//TODO verificar criterio de quebra do método.
+	public void testUploadDocument() throws RemoteException {
+		String documentoToUpload = this.getClass().getResource("/teste.pdf").getPath();
+		
+		String documentID = webService.uploadDocument(documentoToUpload);
+		assertNotNull(documentID);
+		
+		System.out.println("documentID: " + documentID);
+		
+		Boolean disposed = webService.disposeDocument(documentID);
+		assertTrue(disposed);
+	}
+	
+	@Test
+	public void testProcess() throws RemoteException {
+		String documentID = webService.uploadDocument(this.getClass().getResource("/teste.pdf").getPath());
+		assertNotNull(documentID);
+		
+		Boolean disposed = webService.Process(documentID);
+		assertTrue(disposed);
+	}
+	
+	@Test
+	public void testGetServerInformation() throws RemoteException {
+		String serverInformation = webService.getServerInformation();
+		assertNotNull(serverInformation);
 	}
 }
