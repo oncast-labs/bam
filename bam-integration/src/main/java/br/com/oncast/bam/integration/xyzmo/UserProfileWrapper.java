3e7843a8dce254f2b891dd89570e2c2aa1e03fd4
Luiz Avila <lapavila@gmail.com>
2012-08-01 14:11:11 -0300

    Remove addProfile

--- bam-integration/src/main/java/br/com/oncast/bam/integration/xyzmo/UserProfileWrapper.java
+++ bam-integration/src/main/java/br/com/oncast/bam/integration/xyzmo/UserProfileWrapper.java
@@ -35,10 +35,6 @@ public class UserProfileWrapper {
 			throw new UserProfileException(e);
 		}
 	}
-	
-	public void addProfile(String userID, String profileID) {
-		
-	}
 
 	public void deleteUser(String userId) throws UserProfileException {
 		try {
